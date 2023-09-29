fd280b32e770a5299808b50fe99ab7de7f3a0ad1
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-29 16:47:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._Number_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            string op = Console.ReadLine();
+            
+            switch (op)
+            {
+                    case "+":
+                    total = num1 + num2;
+                    break;
+                    case "-":
+                    total = num1 - num2;
+                    break ;
+                    case "*":
+                    total = num1 * num2;
+                    break;
+                    case "/":
+                    total = num1 / num2;
+                    break;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
